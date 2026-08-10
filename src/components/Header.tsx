7ee513f3d5d2013768852974e7f6e12808aa67ac
Erfan Khatamy <mr.erfan.khatami@gmail.com>
2026-08-10 21:58:20 +0330

    added login and loggedout states

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export default function Header() {
   const [isDark, setIsDark] = useState(false);
+  const [isLoggesIn, setIsLoggedIn] = useState(false);
 
   const themeHandler = () => {
     setIsDark(!isDark);
@@ -26,38 +27,54 @@ export default function Header() {
               <Moon className="dark:text-[#FAFAFA]" size={16} />
             )}
           </div>
-          <div className=" items-center justify-around gap-10 h-9 hidden md:flex">
-            <div className="h-9 flex items-center justify-between gap-2 cursor-pointer hover:bg-[#eeeeee] rounded-md px-3 dark:bg-[#0A0A0A] dark:hover:bg-[#262626]">
-              <House size={16} className="dark:text-[#FAFAFA]" />
-              <p className="text-[14px] text-[#171717] dark:text-[#FAFAFA]">
-                Home
-              </p>
-            </div>
-            <div className=" h-9 flex items-center justify-between gap-2 cursor-pointer hover:bg-[#eeeeee] rounded-md px-3 dark:bg-[#0A0A0A] dark:hover:bg-[#262626]">
-              <Bell size={16} className="dark:text-[#FAFAFA]" />
-              <p className="text-[14px] text-[#171717] dark:text-[#FAFAFA]">
-                Notification
-              </p>
-            </div>
+          {isLoggesIn ? (
+            <div className=" items-center justify-around gap-10 h-9 hidden md:flex">
+              <div className="h-9 flex items-center justify-between gap-2 cursor-pointer hover:bg-[#eeeeee] rounded-md px-3 dark:bg-[#0A0A0A] dark:hover:bg-[#262626]">
+                <House size={16} className="dark:text-[#FAFAFA]" />
+                <p className="text-[14px] text-[#171717] dark:text-[#FAFAFA]">
+                  Home
+                </p>
+              </div>
+              <div className=" h-9 flex items-center justify-between gap-2 cursor-pointer hover:bg-[#eeeeee] rounded-md px-3 dark:bg-[#0A0A0A] dark:hover:bg-[#262626]">
+                <Bell size={16} className="dark:text-[#FAFAFA]" />
+                <p className="text-[14px] text-[#171717] dark:text-[#FAFAFA]">
+                  Notification
+                </p>
+              </div>
 
-            <div className="h-9 flex items-center justify-between gap-2 cursor-pointer hover:bg-[#eeeeee] rounded-md px-3 dark:bg-[#0A0A0A] dark:hover:bg-[#262626]">
-              <UsersRound
-                size={16}
-                strokeWidth={1.75}
-                className="dark:text-[#FAFAFA]"
-              />
-              <p className="text-[14px] text-[#171717] dark:text-[#FAFAFA]">
-                Profile
-              </p>
+              <div className="h-9 flex items-center justify-between gap-2 cursor-pointer hover:bg-[#eeeeee] rounded-md px-3 dark:bg-[#0A0A0A] dark:hover:bg-[#262626]">
+                <UsersRound
+                  size={16}
+                  strokeWidth={1.75}
+                  className="dark:text-[#FAFAFA]"
+                />
+                <p className="text-[14px] text-[#171717] dark:text-[#FAFAFA]">
+                  Profile
+                </p>
+              </div>
+              <div className="w-9 h-9 items-center justify-center cursor-pointer hover:bg-[#eeeeee] rounded-md dark:hover:bg-[#262626] hidden md:flex">
+                <LogOut size={16} className="dark:text-[#FAFAFA]" />
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="flex items-center justify-between gap-4">
+              <div className=" h-9 flex items-center justify-between gap-2 cursor-pointer hover:bg-[#17171780] rounded-md px-3 dark:bg-[#0A0A0A] dark:hover:bg-[#262626]">
+                <House size={16} className="dark:text-[#FAFAFA]" />
+                <p className="text-[14px] text-[#171717] dark:text-[#FAFAFA]">
+                  Home
+                </p>
+              </div>
+
+              <div className="h-9 bg-[#0A0A0A] text-white flex items-center justify-between gap-2 cursor-pointer rounded-md px-12  hover:bg-[#17171780]">
+                <p className="text-[14px] text-white dark:text-[#FAFAFA]">
+                  Sign In
+                </p>
+              </div>
+            </div>
+          )}
 
           <div className="md:hidden w-9 h-9 flex items-center justify-center cursor-pointer hover:bg-[#eeeeee] border border-[#E5E5E5] dark:border-[#262626] rounded-md shadow shadow-[#0000001A] dark:hover:bg-[#262626] ">
             <Menu size={16} className="dark:text-[#FAFAFA]" />
-          </div>
-
-          <div className="w-9 h-9 items-center justify-center cursor-pointer hover:bg-[#eeeeee] rounded-md dark:hover:bg-[#262626] hidden md:flex">
-            <LogOut size={16} className="dark:text-[#FAFAFA]" />
           </div>
         </nav>
       </div>
