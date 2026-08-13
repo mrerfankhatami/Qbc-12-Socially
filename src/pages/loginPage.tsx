d5d70f2bb5a1154f7cc15ce610233b152be13233
Narges Malekpour <nargesmalekpour05@gmail.com>
2026-08-13 21:04:00 +0330

    feat: add responsive login card with dark and light mode

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,10 +1,12 @@
 export default function LoginPage() {
   return (
     <div className="min-h-svh flex flex-col items-center justify-center bg-secondary-50 dark:bg-[#262626] px-4 sm:px-6 lg:px-8 py-8">
-      <div>
-        <form className="">
-
-        </form>
+      <div className="flex flex-col gap-6 w-full max-w-sm md:max-w-4xl">
+        <div id="card" className="bg-secondary-0 dark:bg-[#262626] grid grid-cols-1 md:grid-cols-2 p-0 shadow-sm rounded-xl border border-[#E5E5E5] dark:border-[#2e2e2e] overflow-hidden">
+          <form className="p-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolor, explicabo esse delectus quidem minima aliquid harum dolorem distinctio totam aperiam exercitationem quasi nam a cupiditate repellat fugit. Doloremque, nemo.
+          </form>
+        </div>
       </div>
     </div>
   )
