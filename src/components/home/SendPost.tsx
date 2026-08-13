fb35fc57e3e9b18519e2e8e818e93c5a08a5ae19
Erfan Khatamy <mr.erfan.khatami@gmail.com>
2026-08-13 17:03:54 +0330

    send Post componant color Fix

--- src/components/home/SendPost.tsx
+++ src/components/home/SendPost.tsx
@@ -17,7 +17,7 @@ export default function PostInput() {
   return (
     <form
       onSubmit={handleSubmit}
-      className="mx-auto my-6 min-h-60 w-3/5 min-w-50 max-w-150 rounded-2xl border border-[#E3E3E3] bg-white p-6 shadow-[0_2px_5px_rgba(0,0,0,0.08)] dark:border-[#3A3A3A] dark:bg-[#0A0A0A] dark:shadow-[0_2px_5px_rgba(0,0,0,0.25)]"
+      className="mx-auto my-6 min-h-60 w-3/5 min-w-50 max-w-150 rounded-2xl border border-[#E3E3E3]  p-6 shadow-[0_2px_5px_rgba(0,0,0,0.08)] dark:border-[#3A3A3A] dark:bg-[#0A0A0A] dark:shadow-[0_2px_5px_rgba(0,0,0,0.25)]"
     >
       <div className="flex items-start gap-4.5">
         <div className="relative flex size-12.5 shrink-0 items-center justify-center overflow-hidden rounded-full">
@@ -39,7 +39,7 @@ export default function PostInput() {
         <button
           type="submit"
           disabled={!text.trim()}
-          className="flex h-10.5 min-w-25.5 items-center justify-center gap-2 rounded-[7px] bg-[#262626] text-base text-white transition-colors hover:bg-[#171717] disabled:cursor-not-allowed disabled:bg-[#8B8B8B]  dark:bg-white dark:text-black dark:hover:bg-[#E5E5E5] dark:disabled:bg-[#555] dark:disabled:text-[#999]"
+          className="flex h-10.5 min-w-25.5 items-center justify-center gap-2 rounded-[7px] bg-[#262626] text-base text-white transition-colors hover:bg-[#171717] disabled:cursor-not-allowed disabled:bg-grey-700 disabled:text-gray-500  dark:bg-white dark:text-black dark:hover:bg-[#E5E5E5] dark:disabled:bg-[#555] dark:disabled:text-[#999]"
         >
           <Send size={19} strokeWidth={1.8} />
           <span>Post</span>
