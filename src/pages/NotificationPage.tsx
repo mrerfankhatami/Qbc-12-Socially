19243e753eefde8dfd3ff7d7fcafa9f6bb36a6fe
Mohammad Reza Mehdizadeh <morez.mehdizadeh1999@gmail.com>
2026-08-13 11:27:19 +0330

    fixed index.css styles and Notif page height

--- src/pages/NotificationPage.tsx
+++ src/pages/NotificationPage.tsx
@@ -4,11 +4,15 @@ import { notifications } from "../constant/NotificationData";
 
 export default function NotificationPage() {
 
-  const [notificationList, setNotificationList] = useState(notifications);
+  const [notificationList] = useState(notifications);
 
   const unreadCount = notificationList.filter(
     (notification) => !notification.read,
   ).length;
+
+  const handleReadAllNotification = () => {
+    // code
+  }
 
   return (
     <div className="mx-auto flex w-full max-w-5xl flex-col rounded-2xl border border-[#E5E5E5] bg-white shadow-sm dark:border-[#2F2F2F] dark:bg-[#111111]">
@@ -24,21 +28,14 @@ export default function NotificationPage() {
 
           <button
             className="text-sm font-bold text-gray-900 transition-colors hover:text-blue-500 dark:text-white dark:hover:text-blue-400"
-            onClick={() => {
-              setNotificationList((prev) => {
-                return prev.map((item) => ({
-                  ...item,
-                  read: true,
-                }));
-              });
-            }}
+            onClick={() => handleReadAllNotification()}
           >
             Mark all as read
           </button>
         </div>
       </div>
 
-      <ul className="flex max-h-[365px] flex-col gap-3 overflow-y-auto px-5 pb-5 sm:px-6 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-thumb]:bg-gray-600 m-0 list-none p-0">
+      <ul className="flex max-h-102 flex-col gap-3 overflow-y-auto px-5 pb-5 sm:px-6 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-thumb]:bg-gray-600 m-0 list-none p-0">
         {notificationList.map((notification) => (
           <NotificationCard key={notification.id} {...notification} />
         ))}
