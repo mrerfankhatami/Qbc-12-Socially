1e379a0befd601fa2b403e052a697e2e96cc301f
shr80atr <attarshahryar@gmail.com>
2026-08-14 15:13:46 +0330

    rootlayout file is updated.

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -5,7 +5,7 @@ import SideRecommendedUsers from "../components/SideRecommendedUsers";
 import SideProfile from "../components/SideProfile";
 
 export default function RootLayout() {
-  const isAuthenticated = true;
+  const isAuthenticated = false;
 
   const location = useLocation();
 
@@ -14,7 +14,7 @@ export default function RootLayout() {
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-[#0A0A0A]">
       <header className="mx-auto w-[80%]"><Header /></header>
-      <div className="flex mx-auto w-[80%] mt-24 lg:gap-20 md:gap-10">
+      <div className="flex mx-auto w-[80%] mt-24 md:gap-5">
         <aside className={`${ !isAuthenticated ? "hidden md:block" : "hidden" } w-full max-w-84`}><SideSignIn /></aside>
         <aside className={`${ isAuthenticated ? "hidden md:block" : "hidden" } w-full max-w-84`}><SideProfile /></aside>
         <main className="w-full">
