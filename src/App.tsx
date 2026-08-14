6f15c3ff52d5dfa1e1a98c843418ccde42cad302
Erfan Khatamy <mr.erfan.khatami@gmail.com>
2026-08-14 18:25:53 +0330

    merge resolve

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-import AllPosts from "./components/home/AllPosts";
-import SendPost from "./components/home/SendPost";
-
 export default function App() {
-  return (
-    <>
-      <SendPost></SendPost>
-      <AllPosts></AllPosts>
-    </>
-  );
+  return <></>;
 }
