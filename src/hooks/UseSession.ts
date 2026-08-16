f4bf501d2dd839ae38fd48421b5bbb7091c0144a
Erfan Khatamy <mr.erfan.khatami@gmail.com>
2026-08-16 21:45:28 +0330

    minor UseSession changes

--- src/hooks/UseSession.ts
+++ src/hooks/UseSession.ts
@@ -1,2 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import { getSession } from "../services/";
+import { getSession } from "../services/SessionService";
+
+export const useSession = () => {
+  return useQuery({
+    queryKey: ["session"],
+    queryFn: getSession,
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 30,
+    retry: 3,
+    refetchOnReconnect: true,
+  });
+};
