f6bc02699d520e5322d27ce6ff755c4cbacea66a
Erfan Khatamy <mr.erfan.khatami@gmail.com>
2026-08-16 20:10:29 +0330

    added session service

---
+++ src/types/SessionTypes.ts
@@ -0,0 +1,25 @@
+export type SessionResponse = {
+  message: string;
+  success: boolean;
+  data: {
+    session: {
+      expiresAt: string;
+      token: string;
+      createdAt: string;
+      updatedAt: string;
+      ipAddress: string;
+      userAgent: string;
+      userId: string;
+      id: string;
+    };
+    user: {
+      name: string;
+      email: string;
+      emailVerified: boolean;
+      image: string | null;
+      createdAt: string;
+      updatedAt: string;
+      id: string;
+    };
+  };
+};
