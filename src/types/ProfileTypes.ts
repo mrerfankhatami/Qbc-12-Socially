7f3459eac3b74111f00e11a4835a0934522427b3
shr80atr <attarshahryar@gmail.com>
2026-08-16 16:38:05 +0330

    The desired types have been added.

--- src/types/ProfileTypes.ts
+++ src/types/ProfileTypes.ts
@@ -18,3 +18,82 @@ export type UserProfile = {
     followerId: string;
   }[];
 }
+
+export type PostProfile = {
+  id: string;
+  authorId: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+
+  author: {
+    id: string;
+    email: string;
+    image: string | null;
+    name: string;
+  };
+
+  likes: {
+    userId: string;
+  }[];
+
+  comments: {
+    id: string;
+    content: string;
+    author: {
+      id: string;
+      email: string;
+      image: string | null;
+      name: string;
+    };
+    createdAt: string;
+  }[];
+
+  _count: {
+    likes: number;
+    comments: number;
+  };
+}
+
+export type LikedPost = {
+  id: string;
+  userId: string;
+  postId: string;
+  createdAt: string;
+
+  post: {
+    id: string;
+    authorId: string;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
+
+    author: {
+      id: string;
+      email: string;
+      image: string | null;
+      name: string;
+    };
+
+    likes: {
+      userId: string;
+    }[];
+
+    comments: {
+      id: string;
+      content: string;
+      author: {
+        id: string;
+        email: string;
+        image: string | null;
+        name: string;
+      };
+      createdAt: string;
+    }[];
+
+    _count: {
+      likes: number;
+      comments: number;
+    };
+  };
+}
