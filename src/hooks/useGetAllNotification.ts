e497e1eca9bc2fc63f6141a0601377d1b21289a2
Mahan <MahanQaderi1384@gmail.com>
2026-08-18 21:28:56 +0330

    fix: update querykey

--- src/hooks/useGetAllNotification.ts
+++ src/hooks/useGetAllNotification.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllNotifications } from "../services/notificationServices";
+import { useAuthStore } from "../store/authStore";
 
 export const useGetAllNotifications = () => {
 
+  const {user} = useAuthStore()
+
   const query = useQuery({
-    queryKey: ["allNotifications"],
+    queryKey: ["allNotifications" , user?.id],
     queryFn: getAllNotifications,
     retry: false,
     refetchOnWindowFocus: false,
