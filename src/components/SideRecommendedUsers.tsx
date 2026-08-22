2998e25ebfeac07513bef58181acaa93ca581988
Mahan <MahanQaderi1384@gmail.com>
2026-08-22 20:22:58 +0330

    feat: Add link to user profile

--- src/components/SideRecommendedUsers.tsx
+++ src/components/SideRecommendedUsers.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { LoaderCircle } from "lucide-react";
+import avatar from "../assets/avatar.png";
 import { useGetRecommendedUsers } from "../hooks/useGetRecommendedUsers";
 import { useToggleFollowUser } from "../hooks/useToggleFollowUser";
 import type { RecommendedUserTypes } from "../types/RecommendedUserTypes";
+import { Link } from "react-router-dom";
+import { splitUsername } from "../utils/splitUsername";
 
 export const SideRecommendedUsers: React.FC = () => {
   const [followingUserId, setFollowingUserId] = useState<string | null>(null);
@@ -35,52 +38,57 @@ export const SideRecommendedUsers: React.FC = () => {
             <LoaderCircle className="h-6 w-6 animate-spin text-zinc-400" />
           </div>
         ) : (
-          recommendedUsers?.data.map((user: RecommendedUserTypes) => (
-            <div key={user.id} className="flex items-center justify-between">
-              <div className="flex items-center gap-3">
-                <div className="w-10 h-10 rounded-full bg-linear-to-tr from-blue-600 to-indigo-500 flex items-center justify-center text-white overflow-hidden shrink-0">
-                  {user.image ? (
-                    <img
-                      src={user.image}
-                      alt={user.name}
-                      className="w-full h-full object-cover"
-                    />
+          recommendedUsers?.data.map((user: RecommendedUserTypes) => {
+            const userName = splitUsername(user.email);
+
+            return (
+              <div key={user.id} className="flex items-center justify-between">
+                <Link
+                  to={`/profile/${userName}`}
+                  className="flex items-center gap-3"
+                >
+                  <div className="flex h-10 w-10 shrink-0 items-center justify-center overflow-hidden rounded-full bg-linear-to-tr from-blue-600 to-indigo-500 text-white">
+                    {user.image ? (
+                      <img
+                        src={user.image}
+                        alt={user.name}
+                        className="h-full w-full object-cover"
+                      />
+                    ) : (
+                      <img
+                        src={avatar}
+                        alt={`${user.name} avatar`}
+                        className="h-10 w-10 shrink-0 rounded-full object-cover"
+                      />
+                    )}
+                  </div>
+
+                  <div className="flex flex-col">
+                    <span className="text-sm font-semibold text-zinc-900 dark:text-white">
+                      {user.name}
+                    </span>
+
+                    <span className="text-xs text-zinc-500 dark:text-zinc-400">
+                      {user._count.followers} followers
+                    </span>
+                  </div>
+                </Link>
+
+                <button
+                  type="button"
+                  onClick={() => handleFollowToggle(user.id)}
+                  disabled={isFollowingUser && followingUserId === user.id}
+                  className="cursor-pointer rounded-xl border border-zinc-300 px-4 py-1.5 text-xs font-medium text-zinc-900 transition-all disabled:cursor-not-allowed disabled:opacity-60 dark:border-zinc-800 dark:text-white"
+                >
+                  {isFollowingUser && followingUserId === user.id ? (
+                    <LoaderCircle className="h-4 w-4 animate-spin" />
                   ) : (
-                    <svg
-                      className="w-6 h-6 text-white"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-                    </svg>
+                    "Follow"
                   )}
-                </div>
-
-                <div className="flex flex-col">
-                  <span className="text-sm font-semibold text-zinc-900 dark:text-white">
-                    {user.name}
-                  </span>
-
-                  <span className="text-xs text-zinc-500 dark:text-zinc-400">
-                    {user._count.followers} followers
-                  </span>
-                </div>
+                </button>
               </div>
-
-              <button
-                type="button"
-                onClick={() => handleFollowToggle(user.id)}
-                disabled={isFollowingUser && followingUserId === user.id}
-                className="py-1.5 px-4 text-xs font-medium rounded-xl border border-zinc-300 dark:border-zinc-800 text-zinc-900 dark:text-white transition-all cursor-pointer disabled:cursor-not-allowed disabled:opacity-60"
-              >
-                {isFollowingUser && followingUserId === user.id ? (
-                  <LoaderCircle className="h-4 w-4 animate-spin" />
-                ) : (
-                  "Follow"
-                )}
-              </button>
-            </div>
-          ))
+            );
+          })
         )}
       </div>
     </div>
