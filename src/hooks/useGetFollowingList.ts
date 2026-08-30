a4a1a3b75c0c29f35c67523d06ab19028a57a41b
Mahan <MahanQaderi1384@gmail.com>
2026-08-30 20:46:12 +0330

    feat: Add toggle follow in profile page(card details)

--- src/hooks/useGetFollowingList.ts
+++ src/hooks/useGetFollowingList.ts
@@ -2,16 +2,16 @@ import { useQuery } from "@tanstack/react-query";
 import { getFollowingList } from "../services/FollowListService";
 
 export const useGetFollowingList = (
-  id: string,
+  id?: string,
   options?: {
     enabled?: boolean;
   },
 ) => {
   return useQuery({
     queryKey: ["FollowingList", id],
-    queryFn: () => getFollowingList(id),
+    queryFn: () => getFollowingList(id!),
 
-    enabled: options?.enabled,
+    enabled: !!id && (options?.enabled ?? true),
 
     retry: false,
     staleTime: 1000 * 60 * 5,
